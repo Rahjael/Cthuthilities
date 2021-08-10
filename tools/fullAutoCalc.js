16277e92cbd4f9d587efcef295eb26995cf5ec32
Rahjael <surfer.vortex@gmail.com>
2021-08-10 11:36:56 +0200

    added straight inputs to fullauto

--- tools/fullAutoCalc.js
+++ tools/fullAutoCalc.js
@@ -1,3 +1,12 @@
+const FULLAUTOCALC_INPUTS = {
+  weapSkill: 0,
+  targets: [['Target Name', 0]], // [Name, armor]
+  noOfVolleys: 0,
+  wastedDist: 0,
+  volleySize: 3,
+  rangeDifficulty: 0 // 0: normal, 1: hard, 2: extreme, 4: critical, 5: impossible
+};
+
 
 function fullAutoCalc() {
   // Prepare tool container
@@ -6,20 +15,8 @@ function fullAutoCalc() {
   container.id = toolName + '-container';
 
   // Prepare the input container
-  let inputDiv = getInputDiv();
+  let inputDiv = getInputContainer();
   inputDiv.id = toolName + '-input-container';
-
-  const INPUT = {
-    weapSkill: 0,
-    targets: [['Target Name', 0]], // [Name, armor]
-    wastedDist: 0,
-    volleySize: 3,
-    rangeDifficulty: 0 // 0: normal, 1: hard, 2: extreme, 4: critical, 5: impossible
-  };
-
-
-
-
 
 
 
@@ -31,54 +28,82 @@ function fullAutoCalc() {
   return toolData;
 }
 
-function getInputDiv() {
+function getInputContainer() {
   let inputDiv = document.createElement('div');
 
-  inputDiv.append(getWeapSkillInputDiv());
+  inputDiv.append(getInputDiv('Weapon Skill', 'weapSkill', 0, 99, simpleCallback));
+  inputDiv.append(getInputDiv('Wasted Distance', 'wastedDist', 0, 99, simpleCallback));
+  inputDiv.append(getInputDiv('No of Volleys', 'noOfVolleys', 1, 10, simpleCallback));
+  inputDiv.append(getInputDiv('Volley Size', 'volleySize', 3, 10, volleySizeCallback));
+  inputDiv.append(getInputDiv('Range Difficulty', 'rangeDIfficulty', 0, 5, simpleCallback));
 
   return inputDiv;
 }
 
 
-function getWeapSkillInputDiv() {  
-  let weapSkillInputDiv = document.createElement('div');
-  weapSkillInputDiv.classList.add('fullAutoCalc-input-div');
-  let weapSkillInput = document.createElement('input');
-  let weapSkillLabel = document.createElement('label');
-  weapSkillInput.id = 'weapSkill';
-  weapSkillInput.type = 'text';
-  weapSkillInput.max = 99;
-  weapSkillInput.min = 0;
-  weapSkillInput.value = 0;
-  weapSkillInput.classList.add('fullAutoCalc-inputs')
+function getInputDiv(labelText, id, min, max, callback) {
+  let inputDiv = document.createElement('div');
+  inputDiv.classList.add('fullAutoCalc-input-div');
+  let input = document.createElement('input');
+  let label = document.createElement('label');
+  input.id = id;
+  input.type = 'text';
+  input.max = max;
+  input.min = min;
+  input.value = min;
+  input.classList.add('fullAutoCalc-inputs');
 
-  weapSkillLabel.for = weapSkillInput.id;
-  weapSkillLabel.innerText = 'Weapon Skill:';
-  weapSkillLabel.classList.add('fullAutoCalc-labels');
+  label.for = input.id;
+  label.innerText = labelText + ':';
+  label.classList.add('fullAutoCalc-labels');
 
-  weapSkillInput.onchange = (e) => {
-    let value = Number(e.target.value);
-    let max = Number(e.target.max);
-    let min = Number(e.target.min);
+  input.onchange = callback;
 
-    if(value < min || isNaN(value)) {
-      e.target.value = min;
-    }
-    if(value > max) {
-      e.target.value = max;
-    }
-    console.log(value, max, min)
-  };
-
-
-
-
-
-  weapSkillInputDiv.append(weapSkillLabel, weapSkillInput);
-
-
-  return weapSkillInputDiv;
+  inputDiv.append(label, input);
+  return inputDiv;
 }
+function simpleCallback(e) {
+  let value = Number(e.target.value);
+  let max = Number(e.target.max);
+  let min = Number(e.target.min);
+  if(value < min || isNaN(value)) {
+    e.target.value = min;
+  }
+  if(value > max) {
+    e.target.value = max;
+  }
+  FULLAUTOCALC_INPUTS[e.target.id] = e.target.value;
+}
+function volleySizeCallback(e) {
+  let value = Number(e.target.value);
+  let max = Number(Math.floor(document.querySelector('#weapSkill').value / 10));
+  let min = Number(e.target.min);
+  if(value < min || isNaN(value)) {
+    e.target.value = min;
+  }
+  if(value > max) {
+    e.target.value = max;
+  }
+  FULLAUTOCALC_INPUTS[e.target.id] = e.target.value;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function rollRandom(dieNum) {
   return Math.floor(Math.random() * dieNum) + 1;
